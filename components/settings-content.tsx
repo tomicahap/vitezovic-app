24ea1166f941d53f0947c3a468dcd0d99d00f9e0
unknown <tomica.hap@gmail.com>
2026-05-26 20:53:07 +0200

    fix: destructure setGoogleOAuthSettings from useSettings context in SettingsContent component

--- components/settings-content.tsx
+++ components/settings-content.tsx
@@ -56,6 +56,7 @@ export function SettingsContent() {
     removeFunction, 
     setGoogleDriveUrl, 
     setGoogleDriveSettings,
+    setGoogleOAuthSettings,
     setGmailMailbox,
     refreshSettings,
     addMeetingType,
